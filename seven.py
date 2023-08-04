d63ae1ad6b7b123a76a4d0107c9d3e1baf48becd
André Matthies <matthiez@gmail.com>
2023-08-04 10:01:10 +0200

    add python 3 support

--- seven.py
+++ seven.py
@@ -7,7 +7,7 @@
 # This notification script can be put below share/check_mk/notifications.
 # Please configure the needed credentials
 
-import sys, os, urllib
+import sys, os
 
 api_key = os.environ.get('NOTIFY_PARAMETER_1', os.environ.get('SEVEN_API_KEY'))
 sender = os.environ.get('NOTIFY_PARAMETER_2', '')
@@ -22,22 +22,31 @@ if os.environ['NOTIFY_WHAT'] == 'SERVICE':
 else:
     message += "is " + os.environ['NOTIFY_HOSTSTATE'] # Host message
 
-qs = urllib.urlencode({
-    'from': sender,
-    'p': api_key,
-    'text': message,
-    'to': to,
-})
-url = "%s?" % api_url + qs
+try:
+    from urllib.request import Request, urlopen # Python 3
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib2 import Request, urlopen  # Python 2
+    from urllib import urlencode
 
 try:
-    handle = urllib.urlopen(url)
-    response = handle.read().strip()
+    req = Request(api_url)
+    req.add_header('SentWith', 'Checkmk')
+    req.add_header('X-Api-Key', api_key)
 
-    if handle.getcode() == 200:
+    data = urlencode({
+        'from': sender,
+        'text': message,
+        'to': to,
+    }).encode()
+
+    content = urlopen(req, data=data)
+    code = content.getcode()
+    res = int(content.read())
+
+    if code == 200 and res == 100:
         sys.stdout.write("Successfully sent SMS to %s\n" % to)
     else:
-        sys.stderr.write("Error sending SMS to %s: HTTP error code %s\n" % (to, handle.getcode()))
-        sys.stderr.write("URL was %s\n" % url)
+        sys.stderr.write("Error sending SMS to %s: HTTP error code %d, API code %d\n" % (to, code, res))
 except Exception as e:
     sys.stderr.write("Error sending SMS to %s: %s\n" % (to, e))
